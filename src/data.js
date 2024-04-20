572558287b0e9e8016b6892486a43629ce85bfe5
eliangerard <eliangerardgg@gmail.com>
2024-04-20 13:22:42 -0600

    Add dynamic data

---
+++ src/data.js
@@ -0,0 +1,70 @@
+export const data = {
+    me: {
+        name: "Elian Gerard",
+        title: "Fullstack Developer",
+        email: "eliangerardiso@gmail.com",
+        about: "Meticulous developer and engineering student with a profound passion for every facet of software development, specializing in frontend technologies. Fueled by a relentless drive for self-improvement, I am committed to delivering high-quality solutions that exceed expectations."
+    },
+    experience: [
+        {
+            title: "Fullstack Developer",
+            subtitle: "Freelance",
+            data: "2023 - Present",
+            description: "As a freelance Fullstack Developer, I've had the opportunity to work on diverse projects, crafting comprehensive information systems tailored to client needs. One notable project was for TintoTenis, where I developed a web application to streamline their order management process. Additionally, I contributed to Por Amor a Tobby, a web app aimed at aiding animal shelters in Baja California by showcasing animals available for adoption. Alongside these, I've completed various smaller projects, honing my skills and delivering solutions that meet client expectations."
+        },
+        {
+            title: "Programming Tutor",
+            subtitle: "SuperProf",
+            data: "2023 - Present",
+            description: "I've been providing private programming lessons via the SuperProf platform. Through this avenue, I've not only connected with potential clients but also fostered ongoing relationships. I've taught numerous students, maintaining a steady group of learners whom I continue to support. My lessons focus on web programming technologies and their foundational principles.",
+            link: "https://www.superprof.mx/estudiante-ingenieria-ofrece-clases-programacion-html-css-node-react.html",
+        }
+    ],
+    skills: [
+        {
+            title: "Computer Systems Engineer",
+            subtitle: "Tecnológico Nacional de México Campus Chihuahua II",
+            data: "2020 - Present",
+            description: "Focusing on technologies such as Java, Node.js, HTML, CSS, and JavaScript, as well as proficiency in MySQL and MongoDB for database management, deployment experience with cloud platforms like AWS, alongside network administration. Actively involved in competitions and community engagement initiatives in the city.",
+            link: "/Reticula-ISC-2023.webp",
+        },
+        {
+            title: "React (Hooks & MERN)",
+            subtitle: "Udemy - Fernando Herrera",
+            data: "Certification",
+            description: "Learning about Hooks, Redux, Router and Testing, fullstack integration, and more.",
+            link: "https://udemy-certificate.s3.amazonaws.com/pdf/UC-016de529-146c-4e5e-aa3a-7b5924591833.pdf",
+        },
+        {
+            title: "Node.js - Zero to Expert",
+            subtitle: "Udemy - Fernando Herrera",
+            data: "Certification",
+            description: "Learning about Sockets, APIs, Authentication, Deployment, MongoDB, and more.",
+            link: "https://udemy-certificate.s3.amazonaws.com/pdf/UC-c2bbdd5b-64cb-482b-9368-3bdc3cbca820.pdf",
+        },
+    ],
+    projects: [
+        {
+            title: "TintoTenis",
+            description: "Developed a web application for TintoTenis, a local tennis academy. The app streamlines their order management process, allowing users to register for classes, pay for services, and view upcoming events. The app also includes an admin panel for managing user data and generating reports.",
+            image: "/tt.webp",
+        },
+        {
+            title: "Por Amor a Tobby",
+            description: "Contributed to Por Amor a Tobby, a web app aimed at aiding animal shelters in Baja California. The app showcases animals available for adoption, allowing users to view photos, descriptions, and adoption requirements. The app also includes an admin panel for managing animal data and user applications.",
+            image: "/pat.webp",
+            link: "https://poramoratobby.com/",
+        },
+        {
+            title: "NavegaTec",
+            description: "NavegaTec is a web application designed to serve as a map of our university campus, providing students with information about upcoming events and their locations. Collaborating with UI/UX designer Alex, we developed this tool as part of our community service initiative during our time at the university.",
+            image: "/nt.webp",
+            link: "https://navegatest.vercel.app/",
+        },
+        {
+            title: "Simple TTS",
+            description: "A Node.js package that encapsulates the functionality of the google-tts-api package, enabling quick and free text-to-speech conversion. With thousands of downloads, this package has gained popularity for its efficiency and ease of use.",
+            link: "https://www.npmjs.com/package/simple-tts-mp3",
+        }
+    ],
+}
